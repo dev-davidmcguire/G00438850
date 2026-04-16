106accdf772d46cb2ddab2954d28b2b55722559d
dev-davidmcguire <davidmcg.dev@gmail.com>
2026-04-16 07:09:48 +0000

    generated MovieService and added provideHttpClient to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+//No app.config.ts, importing here for now
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -10,5 +12,7 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    // Added HttpClient so the MovieService can make api calls
+    provideHttpClient(),
   ],
 });
