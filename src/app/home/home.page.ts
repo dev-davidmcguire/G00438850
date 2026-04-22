8851c5d2b5dec49f14c91424b4f2ec0a29778d93
dev-davidmcguire <davidmcg.dev@gmail.com>
2026-04-22 07:09:38 +0000

    declared empty movies array, injected movieservice in home constructor, set up getTrending call on page load via ngOnInit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+//Importing MovieService to access API methods.
+import { MovieService } from '../services/movie';
 
 @Component({
   selector: 'app-home',
@@ -12,9 +14,19 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  //Store movies returned from API in an array.
+  movies: any[] = [];
 
+  //MovieService injected via constructor.
+  constructor(private movieService: MovieService) { }
+
+  //ngOnInit runs after the page loads, when the template is ready.
   ngOnInit() {
+    //Call getTrending(), returns observable. we subscribe to it to get the data.
+    //data.results is the array of movies returned.
+    this.movieService.getTrending().subscribe((data: any) => {
+      this.movies = data.results;
+    })
   }
 
 }
