3b2f57d706b6f589694fda1f14694ccd77c5546b
dev-davidmcguire <davidmcg.dev@gmail.com>
2026-04-23 07:01:05 +0000

    added student number and favourites icon to home toolbar. Set up nav to favourites

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonRouterLink, IonIcon } from '@ionic/angular/standalone';
 //Importing MovieService to access API methods.
 import { MovieService } from '../services/movie';
+//Allows for use of icons in the template
+import { addIcons } from 'ionicons';
+import { heart } from 'ionicons/icons';
+//Button navigation didnt work with just IonRouterLink.
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonButton, IonRouterLink, RouterLink, IonIcon]
 })
 export class HomePage implements OnInit {
 
@@ -18,7 +23,11 @@ export class HomePage implements OnInit {
   movies: any[] = [];
 
   //MovieService injected via constructor.
-  constructor(private movieService: MovieService) { }
+  constructor(private movieService: MovieService) { 
+
+    addIcons({heart});
+
+  }
 
   //ngOnInit runs after the page loads, when the template is ready.
   ngOnInit() {
